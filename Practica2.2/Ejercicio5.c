84aa21eff82b2e83a8299fd692748a89a353832d
john <johnibar@ucm.es>
2020-12-19 14:36:37 +0100

    Ejecicio 5

---
+++ Practica2.2/Ejercicio5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(){
+  const char PATH[]="/home/john/Escritorio/ej5.txt";
+
+  int df = open(PATH, O_CREAT, 0645);
+  printf("Descriptor = %i\n", df);
+  return 1;
+}
