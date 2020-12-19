f4c643feeaacef10eb7f1fdc02bc278653a7a6ee
john <johnibar@ucm.es>
2020-12-19 14:58:47 +0100

    Ejercicio6.c

--- Practica2.2/Ejercicio6.c
+++ Practica2.2/Ejercicio6.c
@@ -1,4 +1,12 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
-
+int main(){
+    mode_t m = umask(027); // 027 -> permiso 750
+	int file = open("ej6.txt", O_CREAT | O_RDONLY, 0777);
+	return 0;
+}
